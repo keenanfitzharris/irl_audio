66847576fa80d88d0b2107e612456bce07560f48
keenanfitzharris <68252203+keenanfitzharris@users.noreply.github.com>
2021-03-04 17:39:27 -0500

    Add files via upload

--- manyears_ros/src/global_value.h
+++ manyears_ros/src/global_value.h
@@ -9,10 +9,11 @@ extern "C"{
 namespace manyears_global
 {
 
-	static const int sample_rate_s = GLOBAL_FS; //48000 samples/sec
+	
+	static const int sample_rate_s = 144000; //48000 samples/sec
 	static const int samples_per_frame_s = GLOBAL_FRAMESIZE * GLOBAL_OVERLAP; //512
-	static const int nb_microphones_s = GLOBAL_MICSNUMBER; //8
-	static const unsigned int raw_buffer_size_s = samples_per_frame_s * nb_microphones_s;
+	static const int nb_microphones_s = 7; //7
+	static const unsigned int raw_buffer_size_s = 3584;
 	static const std::string microphones_frame_s = "micro_center_link";
 
 }
